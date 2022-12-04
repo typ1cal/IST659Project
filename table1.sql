e28a0206b8d85612284321c81fd79050ffdf1e2e
typ1cal <yashwadhawe@gmail.com>
2022-12-04 18:52:53 -0500

    solved errors

--- table1.sql
+++ table1.sql
@@ -13,15 +13,29 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
     WHERE CONSTRAINT_NAME = 'fk_clinics_clinic_clinic_id')
     ALTER TABLE CUSTOMERS DROP CONSTRAINT fk_clinics_clinic_clinic_id
 
-
+DROP TABLE IF EXISTS appointments
+DROP TABLE IF EXISTS fix_appointments
+DROP TABLE IF EXISTS employees
+DROP TABLE IF EXISTS clinic_contacts
+DROP TABLE IF EXISTS clinics
+DROP TABLE IF EXISTS state_lookup
 -- UP METADATA
+
+CREATE TABLE state_lookup
+(
+   state_id       INT IDENTITY (1, 1),
+   state_name     VARCHAR (32),
+   state_abbrev   CHAR (2),
+   CONSTRAINT pk_state_lookup_state_id primary key (state_id)
+)
+
 
 CREATE TABLE clinics (
     clinic_id int IDENTITY NOT NULL,
     clinic_name varchar(50) NOT NULL,
     address_line_one varchar (200) NOT NULL,
     address_line_two varchar (200) NOT NULL,
-    clinic_state char(20) NOT NULL,
+    clinic_state int NOT NULL,
     clinic_city char(30) NOT NULL,
     clinic_zip int NOT NULL,
     CONSTRAINT pk_clinics_clinic_id PRIMARY KEY (clinic_id),
@@ -40,8 +54,8 @@ CREATE TABLE clinic_contacts (
     CONSTRAINT pk_clinic_contacts_clinic_id PRIMARY KEY (clinic_id)
 )
 ALTER TABLE clinic_contacts
-    ADD CONSTRAINT fk_clinic_contacts_clinic_id FOREIGN KEY (clinic_contacts)
-        REFERENCES clinic(clinic_id)
+    ADD CONSTRAINT fk_clinic_contacts_clinic_id FOREIGN KEY (clinic_id)
+        REFERENCES clinics(clinic_id)
 
 CREATE TABLE employees(
     employee_id int IDENTITY NOT NULL,
@@ -52,8 +66,16 @@ CREATE TABLE employees(
     CONSTRAINT pk_employees_employee_id PRIMARY KEY (employee_id)
 )
 ALTER TABLE employees
-    ADD CONSTRAINT fk_employees_employee_clinic_id FOREIGN KEY (employees)
-        REFERENCES clinic (clinic_id)
+    ADD CONSTRAINT fk_employees_employee_clinic_id FOREIGN KEY (employee_clinic_id)
+        REFERENCES clinics (clinic_id)
+
+CREATE TABLE appointments(
+    appointment_id INT IDENTITY NOT NULL,
+    appointment_type VARCHAR(255) NOT NULL,
+    appointment_created_date DATETIME NOT NULL,
+    appointment_patient_id INT NOT NULL,
+    CONSTRAINT pk_appointments_appointment_id PRIMARY KEY (appointment_id)
+)
 
 CREATE TABLE fix_appointments(
     fix_appointment_id INT IDENTITY NOT NULL,
@@ -66,25 +88,8 @@ ALTER TABLE fix_appointments
 
 ALTER TABLE fix_appointments
     ADD CONSTRAINT fk_fix_appointments_fix_clinic_id FOREIGN KEY (fix_clinic_id)
-        REFERENCES clinic (clinic_id)
+        REFERENCES clinics (clinic_id)
 
-
-
-CREATE TABLE appointments(
-    appointment_id INT IDENTITY NOT NULL,
-    appointment_type VARCHAR(255) NOT NULL,
-    appointment_created_date DATETIME NOT NULL,
-    appointment_patient_id INT NOT NULL,
-    CONSTRAINT pk_appointments_appointment_id PRIMARY KEY (appointment_id)
-)
-
-CREATE TABLE state_lookup
-(
-   state_id       INT IDENTITY (1, 1),
-   state_name     VARCHAR (32),
-   state_abbrev   CHAR (2),
-   CONSTRAINT pk_state_lookup_state_id primary key (state_id)
-)
 
 --UP DATA
 
